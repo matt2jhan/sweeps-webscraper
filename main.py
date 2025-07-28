c6f01cc9789a86ce0c1003368d1499f5350ae501
matthewjyhan <176155858+matthewjyhan@users.noreply.github.com>
2025-07-27 21:15:33 -0400

    Hosting on Render

--- main.py
+++ main.py
@@ -114,4 +114,4 @@ def logout():
     return redirect(url_for("login"))
 
 if __name__ == "__main__":
-    app.run(debug=True) # Remember to use flask run later
+    app.run(host='0.0.0.0', port=10000)
